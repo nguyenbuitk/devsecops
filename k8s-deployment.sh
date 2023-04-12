bab044c1a92041fd62fac8095d74d6fb711ffc69
nguyenbuitk <buinguyen23112k@gmail.com>
2023-04-12 16:42:38 +0000

    fixing k8s deployment

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -3,16 +3,16 @@
 #
 sed -i "s#replace#${imageName}#g" k8s_deployment_service.yaml
 
-## Lỗi k8s pod không take effect khi sử dụng đoạn bash script này, chỉ có image là thay đổi
-kubectl -n default get deployment ${deploymentName} > /dev/null
+# ## Lỗi k8s pod không take effect khi sử dụng đoạn bash script này, chỉ có image là thay đổi
+# kubectl -n default get deployment ${deploymentName} > /dev/null
 
-#  output command = 1
-if [[ $? -ne 0 ]]; then
-    echo "deployment ${deploymentName} doesnt exist"
-    kubectl -n default apply -f k8s_deployment_service.yaml
-else
-    echo "deployment ${deployment} exist"
-    echo "image name - ${imageName}"
-    kubectl -n default set image deploy ${deploymentName} ${containerName}=${imageName} --record=true
-fi
+# #  output command = 1
+# if [[ $? -ne 0 ]]; then
+#     echo "deployment ${deploymentName} doesnt exist"
+#     kubectl -n default apply -f k8s_deployment_service.yaml
+# else
+#     echo "deployment ${deployment} exist"
+#     echo "image name - ${imageName}"
+#     kubectl -n default set image deploy ${deploymentName} ${containerName}=${imageName} --record=true
+# fi
 kubectl -n default apply -f k8s_deployment_service.yaml
