077633aea3175f64ce439277c67815b240839cdc
nguyenbuitk <buinguyen23112k@gmail.com>
2023-04-16 14:45:13 +0000

    fix error in rollout

--- k8s-deployment-rollout-status.sh
+++ k8s-deployment-rollout-status.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # check the status of a Kubernetes Deployment rollout and report if the rollout has succeeded or failed.
 # đảm bảo application được deploy không có lỗi và tránh những issue như downtime hoặc application crash
-ensure that your application is deployed correctly and avoid issues like downtime or application crashes caused by failed deployments
+# ensure that your application is deployed correctly and avoid issues like downtime or application crashes caused by failed deployments
 sleep 10s
 
 if [[ $(kubectl -n default rollout status deploy ${deploymentName} --timeout 5s) != *"successfully rolled out"* ]]; then
