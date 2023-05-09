a2450a0e222ee3c6850eaf0be757a25eeb1b6a08
nguyenbuitk <buinguyen23112k@gmail.com>
2023-05-09 16:28:42 +0000

    config for opa_conftest fail intentionally

--- opa-conftest-check.sh
+++ opa-conftest-check.sh
@@ -1,8 +1,9 @@
 opa_output=$(docker run --rm -v $(pwd):/project openpolicyagent/conftest test --policy opa-docker-security.rego Dockerfile)
+exit_code=$?
 echo $opa_output
-number_of_failures=$(echo $opa_output | grep -oP '\d+ failures' | awk '{print $1}' | cut -d' ' -f1)
+# number_of_failures=$(echo $opa_output | grep -oP '\d+ failures' | awk '{print $1}' | cut -d' ' -f1)
 
-if (( number_of_failures > 0 )); then
+if [[ "${exit_code}" == 1 ]]; then
   echo "OPA Conftest found policy violations"
   exit 1
 else
