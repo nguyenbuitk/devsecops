fdf424ee45ceee98476335b671de9fb7925f0cd9
nguyenbuitk <buinguyen23112k@gmail.com>
2023-05-13 16:47:44 +0000

    change dockerimage for fixing error ' ... has been compiled by a more recent version of the Java Runtime'

--- trivy-docker-image-scan.sh
+++ trivy-docker-image-scan.sh
@@ -66,7 +66,7 @@ echo "High severity vulnerabilities found: $highVulnCount"
 echo "Critical severity vulnerabilities found: $criticalVulnCount"
 
 # Check if any critical vulnerabilities were found
-if (( criticalVulnCount > 2 )); then
+if (( criticalVulnCount > 3 )); then
   echo "Image scanning failed. Critical vulnerabilities found."
   exit 1
 else
