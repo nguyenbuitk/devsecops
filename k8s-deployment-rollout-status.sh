105b528546a74da2af4f7d3d503193ce86ea6616
nguyenbuitk <buinguyen23112k@gmail.com>
2023-05-13 15:38:09 +0000

    test mvn new version

--- k8s-deployment-rollout-status.sh
+++ k8s-deployment-rollout-status.sh
@@ -2,6 +2,7 @@
 # check the status of a Kubernetes Deployment rollout and report if the rollout has succeeded or failed.
 # ensure that your application is deployed correctly and avoid issues like downtime or application crashes caused by failed deployments
 sleep 10s
+deploymentName=devsecops
 if [[ $(kubectl -n default rollout status deploy "${deploymentName}" --timeout 60s) != *"deployment \""${deploymentName}"\" successfully rolled out"* ]]; then
     echo "Deployment ${deploymentName} Rollout has Failed"
     kubectl -n default rollout undo deploy ${deploymentName}
