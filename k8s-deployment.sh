c74a33745979f9858512e49cfa61be15d30e72dc
nguyenbuitk <buinguyen23112k@gmail.com>
2023-05-17 18:43:06 +0000

    update k8s-deployment file

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 #
-sed -i "s#replace#${imageName}#g" k8s_deployment_service.yaml
+# sed -i "s#replace#${imageName}#g" k8s_deployment_service.yaml
+# # kubectl -n default apply -f k8s_deployment_service.yaml
 
-# ## Error bash script don't take effect when apply this 
+# ## Error bash script don't take effect when apply this
 # kubectl -n default get deployment ${deploymentName} > /dev/null
 
 # #  output command = 1
@@ -15,4 +16,26 @@ sed -i "s#replace#${imageName}#g" k8s_deployment_service.yaml
 #     echo "image name - ${imageName}"
 #     kubectl -n default set image deploy ${deploymentName} ${containerName}=${imageName} --record=true
 # fi
-kubectl -n default apply -f k8s_deployment_service.yaml
+#!/bin/bash
+
+deploymentFile="k8s_deployment_service.yaml"
+namespace="default"
+timestamp=$(date +%Y%m%d%H%M%S)
+
+# Backup the original deployment file
+backupFile="${deploymentFile}.bak.${timestamp}"
+cp "${deploymentFile}" "${backupFile}"
+
+# Replace the placeholder with the image name
+sed -i "s#replace#${imageName}#g" "${deploymentFile}"
+
+# Apply the modified deployment file to the Kubernetes cluster
+kubectl -n "${namespace}" apply -f "${deploymentFile}"
+
+if [ $? -eq 0 ]; then
+  echo "Deployment completed successfully."
+else
+  echo "Deployment failed."
+  echo "Restoring the original deployment file..."
+  mv "${backupFile}" "${deploymentFile}"
+fi
